f49300df43063043a83768fcafe8d2dbb9763ea1
milosmilivojevic1401@ <mmilivojevic1401@gmail.com>
2024-07-27 19:40:27 +0200

    osnovi program koji pokazuje rad sa nitima

---
+++ novaVezba/src/novaVezba/Main.java
@@ -0,0 +1,18 @@
+package novaVezba;
+
+public class Main {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		
+		
+		
+		for(int i = 0; i<100;i++) {
+			
+			System.out.println(i);
+			Thread.sleep(3000);
+		}
+
+	}
+
+}
