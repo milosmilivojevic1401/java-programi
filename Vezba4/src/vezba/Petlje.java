49e6d95b6e4f975b76b8cc5f6288b0aacafff094
milosmilivojevic1401@ <mmilivojevic1401@gmail.com>
2024-07-27 18:26:44 +0200

    dadata for petlja

---
+++ Vezba4/src/vezba/Petlje.java
@@ -0,0 +1,17 @@
+package vezba;
+
+
+// ovaj program ilustruje osnovni pojam for petlje 
+public class Petlje {
+	
+	
+	public static void main(String args) {
+		
+		
+		for(int i = 0; i<10;i++) {
+			
+			System.out.println(i);
+		}
+	}
+
+}
