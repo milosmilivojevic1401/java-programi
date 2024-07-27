c5d51dc6a348862078213178117b0c8ac428fcea
milosmilivojevic1401@ <mmilivojevic1401@gmail.com>
2024-07-27 19:46:12 +0200

    program koji konstruise referencu i objekat fudbaler

---
+++ vezba5/src/vezba5/Fudbaler.java
@@ -0,0 +1,15 @@
+package vezba5;
+
+public class Fudbaler {
+
+	
+	public String ime;
+	public String klub;
+	@Override
+	public String toString() {
+		return "Fudbaler [ime=" + ime + ", klub=" + klub + "]";
+	}
+	
+	
+	
+}
---
+++ vezba5/src/vezba5/Main.java
@@ -0,0 +1,16 @@
+package vezba5;
+
+public class Main {
+
+	public static void main(String[] args) {
+	
+  Fudbaler f1 = new Fudbaler();
+  
+  f1.ime = "Ronaldo";
+  f1.klub = "Mancester";
+  
+  
+  System.out.println(f1);
+	}
+
+}
